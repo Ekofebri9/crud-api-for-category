576431604d27663d72290b8790a8e4d9b85fa7ae
febri <ekofebri.tech@gmail.com>
2026-01-23 08:11:44 +0700

    add api create category

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,7 +24,8 @@ func main() {
 	})
 
 	http.HandleFunc("/api/categories", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
+		switch r.Method {
+		case http.MethodGet:
 			w.Header().Set("Content-Type", "application/json")
 			err := json.NewEncoder(w).Encode(categories)
 			if err != nil {
@@ -32,7 +33,19 @@ func main() {
 				return
 			}
 			w.WriteHeader(http.StatusOK)
-		} else {
+		case http.MethodPost:
+			var newCategory models.Category
+			err := json.NewDecoder(r.Body).Decode(&newCategory)
+			if err != nil {
+				http.Error(w, "Invalid request payload", http.StatusBadRequest)
+				return
+			}
+			newCategory.ID = len(categories) + 1
+			categories = append(categories, newCategory)
+			w.Header().Set("Content-Type", "application/json")
+			_ = json.NewEncoder(w).Encode(newCategory)
+			w.WriteHeader(http.StatusCreated)
+		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
